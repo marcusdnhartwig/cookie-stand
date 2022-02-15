8a9a8b01c6cdfd5b25a892fd9c8727ae131bcdff
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-02-15 09:03:52 -0800

    cont

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('howdy');
 
-let openHours = ['0600', '0700']
+let openHours = ['0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', `1600`, '1700', '1800', '1900', '2000',]
 
 let seattle = {
   location : 'Seattle',
@@ -21,17 +21,17 @@ let seattle = {
     for (let i= 0; i < openHours.length; i++)
     this.cookiesPerHr.push(Math.ceil(this.custPerHr[i] * this.avgSales));
     console.log(this.cookiesPerHr);
-  },
-   cityName [seattle];
-
-  function renderAllcities(){
-    for(let i = 0; i < cityName.length; i++){
-      let 
     }
   }
-}
 
-//const Stores = [seattle]
+  // finalSale: render() {
+  //   for (let i = 0; i < openHours.length; i++;){
+  //     this.totalCookies = (Math.ceil(this.cookiesPerHr[i] + this.totalCookies))[0];
+  //     console.log(this.totalCookies);
+  //   }
+
+
+const Stores = [seattle]
 
 seattle.calcCookies();
 seattle.render();
