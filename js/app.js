e83ea3e35ea4f3e325a6c2b8ae7ad45a20450a59
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-02-16 17:20:12 -0800

    bad marcus

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('howdy');
 
-let storeHours = ['0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', `1600`, '1700', '1800', '1900',];  
+let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];  
 
 function randomCustomerNum (min, max){
   return Math.floor(Math.random() * (max - min + 1) + min); 
